8bc384e0b9ea3862043fdaec83ac943731556ed3
Alex Cuello ortiz <acuellor.6@gmail.com>
2017-05-12 17:12:28 +0200

    First commit

---
+++ Example/Koloda/BackgroundAnimationViewController.swift
@@ -0,0 +1,134 @@
+//
+//  BackgroundAnimationViewController.swift
+//  Koloda
+//
+//  Created by Eugene Andreyev on 7/11/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import Koloda
+import pop
+
+private let numberOfCards: Int = 5
+private let frameAnimationSpringBounciness: CGFloat = 9
+private let frameAnimationSpringSpeed: CGFloat = 16
+private let kolodaCountOfVisibleCards = 2
+private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
+
+class BackgroundAnimationViewController: UIViewController {
+
+    @IBOutlet var customView: UIView!
+    @IBOutlet weak var kolodaView: CustomKolodaView!
+    
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        kolodaView.alphaValueSemiTransparent = kolodaAlphaValueSemiTransparent
+        kolodaView.countOfVisibleCards = kolodaCountOfVisibleCards
+        kolodaView.delegate = self
+        kolodaView.dataSource = self
+        kolodaView.animator = BackgroundKolodaAnimator(koloda: kolodaView)
+        
+        self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+    }
+    
+    
+    //MARK: IBActions
+    @IBAction func leftButtonTapped() {
+        kolodaView?.swipe(.left)
+    }
+    
+    @IBAction func rightButtonTapped() {
+        kolodaView?.swipe(.right)
+    }
+    
+    @IBAction func undoButtonTapped() {
+        kolodaView?.revertAction()
+    }
+}
+
+//MARK: KolodaViewDelegate
+extension BackgroundAnimationViewController: KolodaViewDelegate {
+    
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        kolodaView.resetCurrentCardIndex()
+    }
+    
+    func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
+        UIApplication.shared.openURL(URL(string: "https://yalantis.com/")!)
+    }
+    
+    func kolodaShouldApplyAppearAnimation(_ koloda: KolodaView) -> Bool {
+        return true
+    }
+    
+    func kolodaShouldMoveBackgroundCard(_ koloda: KolodaView) -> Bool {
+        return false
+    }
+    
+    func kolodaShouldTransparentizeNextCard(_ koloda: KolodaView) -> Bool {
+        return true
+    }
+    
+    func koloda(kolodaBackgroundCardAnimation koloda: KolodaView) -> POPPropertyAnimation? {
+        let animation = POPSpringAnimation(propertyNamed: kPOPViewFrame)
+        animation?.springBounciness = frameAnimationSpringBounciness
+        animation?.springSpeed = frameAnimationSpringSpeed
+        return animation
+    }
+}
+
+// MARK: KolodaViewDataSource
+extension BackgroundAnimationViewController: KolodaViewDataSource {
+    
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        return .default
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return numberOfCards
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        
+        let imageView = UIImageView(image: UIImage(named: "cards_\(index + 1)"))
+        
+        
+        let newView = UIView()
+        newView.backgroundColor = UIColor.black
+        newView.alpha = 0.5
+        newView.layer.cornerRadius = 8
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.addSubview(newView)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: imageView, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 0)
+        
+        let verticalConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: imageView, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 0)
+        
+        let verticalConstraint2 = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: imageView, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
+        
+        
+        let widthConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
+        let heightConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
+        
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, verticalConstraint2, widthConstraint, heightConstraint])
+        
+        let blurEffect = UIBlurEffect(style: .light)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        newView.addSubview(visualEffectView)
+        
+        let label = UILabel() //frame: CGRect(x: (imageView.frame.maxX - imageView.frame.minX)/2 - 25, y: (imageView.frame.maxY - 100), width: 100, height: 100)
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+        
+        label.textColor = UIColor.red
+        label.layer.zPosition = 1
+//        imageView.addSubview(label)
+        
+        return imageView
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
+        return Bundle.main.loadNibNamed("CustomOverlayView", owner: self, options: nil)?[0] as? OverlayView
+    }
+}
